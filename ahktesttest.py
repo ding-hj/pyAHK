f99b788fa4b6e55713d7b6fd3ee7d778461b5ad0
Xelnect <dido.jordanov7@gmail.com>
2015-07-21 00:52:45 +0300

    working wth some functionality

--- ahktesttest.py
+++ ahktesttest.py
@@ -1,7 +1,22 @@
-__author__ = 'XelnectPC'
-from pywinauto.application import Application
-app = Application.start("notepad.exe")
-app.Notepad.Edit
-app.Notepad.Edit.TypeKeys("Hello World ")
+import pywinauto
+import time
+
+pwa_app = pywinauto.application.Application()
+pwa_app.start_('notepad.exe')
+ 
 #Type some keys into the editbox
-app.UntitledNotepad.Edit.TypeKeys("test{SPACE}from{SPACE}pywinauto")
+pwa_app.UntitledNotepad.Edit.TypeKeys("test{SPACE}from{SPACE}pywinauto")
+ 
+#Get handle to the window
+w_handle = pywinauto.findwindows.find_windows(title=u'Untitled - Notepad', class_name='Notepad')[0]
+window = pwa_app.window_(handle=w_handle)
+
+time.sleep(5) # delays for 5 seconds
+#Get text from the edit box
+edit = window['Edit']
+props = edit.GetProperties()
+print props['Texts']
+
+time.sleep(5) # delays for 5 seconds
+#Trigger the About menu item
+window.MenuItem(u'&Help->&About Notepad').Click()
