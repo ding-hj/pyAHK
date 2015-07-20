ef388d5f86242b56ef567a3f1a6e478dd10f7556
Xelnect <dido.jordanov7@gmail.com>
2015-07-21 00:41:18 +0300

    added stuff

--- ahktesttest.py
+++ ahktesttest.py
@@ -2,4 +2,6 @@ __author__ = 'XelnectPC'
 from pywinauto.application import Application
 app = Application.start("notepad.exe")
 app.Notepad.Edit
-app.Notepad.Edit.TypeKeys("Hello World")
+app.Notepad.Edit.TypeKeys("Hello World ")
+#Type some keys into the editbox
+app.UntitledNotepad.Edit.TypeKeys("test{SPACE}from{SPACE}pywinauto")
